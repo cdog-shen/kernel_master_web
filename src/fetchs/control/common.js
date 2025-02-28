56b4c1e7f5bad85350d683fd3e26a780ca7006f4
cdog-shen <shencdog@outlook.com>
2025-02-28 15:20:10 +0800

    add: bing daily API fetch

--- src/fetchs/control/common.js
+++ src/fetchs/control/common.js
@@ -1,3 +1,34 @@
+export async function BingDaily() {
+  try {
+    const resp = await fetch(
+      "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+    );
+    console.log(await resp.json());
+
+    if (!resp.ok) {
+      return {
+        code: 500,
+        msg: "ImageArchive response error",
+        data: await resp.text(),
+      };
+    }
+    const data = await resp.json();
+    const url = `https://cn.bing.com${data.images[0].url}`;
+    return {
+      code: 200,
+      msg: "Bing daily image fetch success",
+      data: url,
+    };
+  } catch (error) {
+    console.error("Error fetching bing daily image:", error);
+    return {
+      code: 500,
+      msg: "ImageArchive connection error",
+      data: String(error),
+    };
+  }
+}
+
 export async function FetchUsi(uid, jwt) {
   try {
     const resp = await fetch(`${process.env.BACKEND_BASE_URL}/auth/me/${uid}`, {
