a2cd1becd8af5358f92fdb184032f6644efa960a
cdog-shen <shencdog@outlook.com>
2025-03-29 14:26:34 +0800

    add: Added data formatting utility functions to support converting JSON to key-value pair arrays and table metadata.

---
+++ src/util/data_format.ts
@@ -0,0 +1,55 @@
+import { GridColDef } from "@mui/x-data-grid";
+
+export function transformJsonToKeyValueArray(json: Record<string, any>): {
+  k: string;
+  v:
+    | Record<string, any>
+    | string
+    | number
+    | boolean
+    | null
+    | ReturnType<typeof transformJsonToKeyValueArray>;
+}[] {
+  return Object.entries(json).map(([key, value]: [string, any]) => {
+    if (Array.isArray(value)) {
+      if (value.length === 0) {
+        return { k: key, v: "Empty array" };
+      }
+      return {
+        k: key,
+        v: value.map((item, index) => ({ key: index, v: item })),
+      };
+    } else if (typeof value === "object" && value !== null) {
+      return { k: key, v: transformJsonToKeyValueArray(value) };
+    }
+    return { k: key, v: value };
+  });
+}
+
+export function transformJson2TableMeta(
+  json: Record<string, any>,
+  headerMap: Record<string, string>
+): GridColDef[] {
+  return Object.keys(json).map((key) => ({
+    field: key,
+    headerName: headerMap[key] || key,
+    width: 150,
+    type: ["string", "number", "boolean", "date"].includes(typeof json[key])
+      ? (typeof json[key] as GridColDef["type"])
+      : undefined,
+    flex: 1,
+  }));
+}
+
+export function transformJson2TableData(
+  json: Record<string, any>,
+  col: GridColDef[]
+): Record<string, any>[] {
+  return json.map((item: Record<string, any>) => {
+    const row: Record<string, any> = {};
+    col.forEach((column) => {
+      row[column.field] = item[column.field];
+    });
+    return row;
+  });
+}
